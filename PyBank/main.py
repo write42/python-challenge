8696d03fad3f46c0b68c64bceb930fe55921b182
Jade <jadeve.jimenez@yahoo.com>
2021-01-07 20:44:14 -0800

    Can't connect with Resources folder, Second commit

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import csv
 bank_csv = os.path.join('..','Resources','03-Python_Homework_Instruction_PyBank_Resources_budget_data')
 
@@ -9,12 +10,12 @@ def budget(banks):
     total_months += months 
     total_profit += profit 
     profit_avg = total_profit/total_months
-    profit_inc =
-    profit_dec = 
+    profit_inc = max(csv_header, key= lambda row: int(row[1])) 
+    profit_dec = min(csv_header, key= lambda row: int(row[1]))  
     return
 
 with open(bank_csv, 'r') as csvfile:
     csvreader = csv.reader(csvfile,delimiter=',')
-    header = next(csvreader)
+    csv_header = next(csvreader) 
     for row in csvreader:
         budget()
