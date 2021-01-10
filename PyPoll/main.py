ec25ea219202d93f2296dd35a3639cb0322931b3
Jade <jadeve.jimenez@yahoo.com>
2021-01-09 17:28:39 -0800

    Third commit, need calc vote_per

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,16 +1,23 @@
 import os
 import csv
-poll_csv = os.path.join('Resources','03-Python_Homework_Instructions_PyPoll_Resources_election_data')
+poll_csv = os.path.join('Resources','03-Python_Homework_Instructions_PyPoll_Resources_election_data.csv')
 
 def polls(votes):
     total_votes = num_rows
     candidate = str(votes[2])
-    can_total = 
-    vote_per = (can_total/total_votes) * 100
-    winner =
+    #vote_per = (can_total/total_votes) * 100
+    if candidate =="Khan":
+        print(f"{candidate}: {vote_per}% ()")
+    elif candidate =="Correy":
+            print(f"{candidate}: {vote_per}% ()")
+    elif candidate == "Li":
+                print(f"{candidate}: {vote_per}% ()")
+    elif candidate == "O'Tooley":
+                    print(f"{candidate} :{vote_per}% ()")
+
 
 with open(poll_csv,'r') as csvfile:
-    csvreader = csv.reader(csvfile, delimiter',')
+    csvreader = csv.reader(csvfile, delimiter=',')
     csv_header = next(csvreader)
     num_rows = 0
     for row in csvreader:
