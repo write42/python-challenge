117bc52e42d6cf63cc184f4b9eff895b53253375
Jade <jadeve.jimenez@yahoo.com>
2021-01-09 17:03:30 -0800

    Sixth commit, need max and min help

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,34 +3,40 @@ import csv
 bank_csv = os.path.join('Resources','03-Python_Homework_Instructions_PyBank_Resources_budget_data.csv')
 
 def budget(banks):
-    months = str(banks[0])
-    profit = float(banks[1])
-    profit_inc = 0
-    profit_dec = 0
+     months = str(banks[0])
+     profit = int(banks[1])
+     profit_inc = int(banks[1])
+     profit_dec = int(banks[1])
+     num_rows = 0
+     for row in csvreader:
+         num_rows +=1
 
-    total_months = num_rows 
-    total_profit = sum(profit for r in csv.reader(csv_header)) 
-    profit_avg = total_profit/total_months
+     total_months = num_rows 
+     total_profit = sum(profit for r in csv.reader(csv_header)) 
+     profit_avg = total_profit/total_months
 
-    for line in banks:
-        if profit > profit_inc:
-           profit_inc= profit
-        if profit < profit_dec:
-            profit_dec =profit
+     for i in banks:
+         if profit > profit_inc:
+            profit_inc = profit[i]
+            increase_month = months[i]
+         elif profit < profit_dec:
+             profit_dec = profit[i]
+             decrease_month = months[i]
 
-    print("Financial Analysis")
-    print("-----------------------------")
-    print(f"Total Months: {total_months}")
-    print(f"Total: {total_profit}")
-    print(f"Average change: {profit_avg}")
-    print(f"Greatest Increase in Profits: {months} ({profit_inc})")
-    print(f"Greatest Decrease in Profits: {months} ({profit_dec})") 
-return
+        
+
+     print("Financial Analysis")
+     print("-----------------------------")
+     print(f"Total Months: {total_months}")
+     print(f"Total: {total_profit}")
+     print(f"Average change: {profit_avg}")
+     print(f"Greatest Increase in Profits: {increase_month} ({profit_inc})")
+     print(f"Greatest Decrease in Profits: {decrease_month} ({profit_dec})") 
 
 with open(bank_csv, 'r') as csvfile:
-    csvreader = csv.reader(csvfile,delimiter=',')
-    csv_header = next(csvreader) 
-    num_rows=0
-    for row in csvreader:
-        num_rows +=1
-        budget(row)
+     csvreader = csv.reader(csvfile,delimiter=',')
+     csv_header = next(csvreader) 
+     num_rows=0
+     for row in csvreader:
+        
+         budget(row)
