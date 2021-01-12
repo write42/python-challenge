5c6964924700fee1968e89a6575a26e9973386e7
Jade <jadeve.jimenez@yahoo.com>
2021-01-11 21:47:20 -0800

    Final Commit, done

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,13 +1,13 @@
 import os
 import csv
 bank_csv = os.path.join('Resources','03-Python_Homework_Instructions_PyBank_Resources_budget_data.csv')
+analysis_file = os.path.join('Analysis','PyBank_analysis.txt')
 num_rows = 0
 
 with open(bank_csv, 'r') as csvfile:
     csvreader = csv.reader(csvfile,delimiter=',')
     csv_header = next(csvreader) 
     total_profit = 0
-    #max_profit, min_profit = [], []
     profits = []
     date = []
     profits_change = []
@@ -16,16 +16,7 @@ with open(bank_csv, 'r') as csvfile:
         num_rows +=1
         date.append(row[0])
         profits.append(float(row[1]))
-        #print(" ~ file: main.py ~ line 40 ~ row", row)     
-        # print("~ file:main.py ~ line 6 ~ banks",banks)
-        #total_profit += float(row[1])
-        #print(total_profit)
-        #profit_avg = 
-        #print(profit_avg)
-        #print(profit_avg)
-        #print(num_rows)
-        #max_profit.append(row[1])
-        #min_profit.append(row[1])
+
     for i in range(1,len(profits)):
         profits_change.append(profits[i]-profits[i-1])
         avg_change = sum(profits_change)/len(profits_change)
@@ -34,7 +25,15 @@ with open(bank_csv, 'r') as csvfile:
         max_date = str(date[profits_change.index(max(profits_change))])
         min_date = str(date[profits_change.index(min(profits_change))])
 
-
+report = f"""
+Financial Analysis
+------------------------
+Total Months: {num_rows}
+Total: {sum(profits)}
+Average Change: {avg_change}
+Greatest Increase in Profits: {max_date} ({max_avg_change})
+eatest Decrease in Profits: {min_date} ({min_avg_change})
+"""
 print("Financial Analysis")
 print("------------------------")
 print(f"Total Months: {num_rows}")
@@ -42,14 +41,5 @@ print(f"Total: {sum(profits)}")
 print(f"Average Change: {avg_change}")
 print(f"Greatest Increase in Profits: {max_date} ({max_avg_change})")
 print(f"Greatest Decrease in Profits: {min_date} ({min_avg_change})")
-    #  for i in csvreader:
-    #      print(" ~ file: main.py ~ line 31 ~ i", i)
-    #      print(" ~ file: main.py ~ line 33 ~ profit", profit)
-    #      print(" ~ file: main.py ~ line 34 ~ profit_inc", profit_inc)
-
-    #      if profit > profit_inc:
-    #         profit_inc = profit[i]
-    #         increase_month = months[i]
-    #      elif profit < profit_dec:
-    #         profit_dec = profit[i]
-    #         decrease_month = months[i]
+with open(analysis_file, 'w') as f:
+    f.write(report)
