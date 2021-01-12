8fd5b8449715ec9a0481a5b20087d954d16ecfcd
Jade <jadeve.jimenez@yahoo.com>
2021-01-11 22:32:12 -0800

    Final Commit

--- PyBank/main.py
+++ PyBank/main.py
@@ -18,10 +18,14 @@ with open(bank_csv, 'r') as csvfile:
         profits.append(float(row[1]))
 
     for i in range(1,len(profits)):
+        #Calculate change between one row and the previous one and fill list
         profits_change.append(profits[i]-profits[i-1])
+        #Calculate average change between months
         avg_change = sum(profits_change)/len(profits_change)
+        #Find the max and min of changes in profits
         max_avg_change = max(profits_change)
         min_avg_change = min(profits_change)
+        #In date list, run profits_change and find the index of the date of the greates max and min in profits, then convert to string
         max_date = str(date[profits_change.index(max(profits_change))])
         min_date = str(date[profits_change.index(min(profits_change))])
 
@@ -32,7 +36,7 @@ Total Months: {num_rows}
 Total: {sum(profits)}
 Average Change: {avg_change}
 Greatest Increase in Profits: {max_date} ({max_avg_change})
-eatest Decrease in Profits: {min_date} ({min_avg_change})
+Greatest Decrease in Profits: {min_date} ({min_avg_change})
 """
 print("Financial Analysis")
 print("------------------------")
